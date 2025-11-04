59debd98bfff1302d0366e27684b1facda2d3845
centrix1337 <vincent.braunschlaeger@gmail.com>
2025-11-04 15:17:48 +0100

    changed url

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,6 +1,6 @@
 export const site = {
     name: "SciCount Fitness",
-    url: process.env.NEXT_PUBLIC_SITE_URL || "https://sci-count-fitness-9mot.vercel.app/",
+    url: process.env.NEXT_PUBLIC_SITE_URL || "https://scicountfitness.de",
     description: "Evidenzbasierter Fitness-Blog – Technik, Training & Ernährung.",
     twitter: "@dein_instagram_oder_twitter" // optional
 };
